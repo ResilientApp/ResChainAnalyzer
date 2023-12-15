d4e85c10b2970297fd8694b972db496e549cff1e
root <root@res-db-main.c.resdb-chain-explorer.internal>
2023-12-15 08:06:34 +0000

    app.py changes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, redirect
 import requests
 
-IP_ADDRESS_TO_TRANSACTIONS = 'http://127.0.0.1:18000/v1/transactions'
-IP_ADDRESS_TO_HOSTING_SITE = 'http://34.29.209.247'
+IP_ADDRESS_TO_TRANSACTIONS = 'http://crow.resilientdb.com/v1/transactions'
+IP_ADDRESS_TO_HOSTING_SITE = 'http://104.198.198.56'
 
 
 app = Flask(__name__)
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def home():
     response = requests.get(IP_ADDRESS_TO_TRANSACTIONS)
     with open('/var/www/html/ResDB-Chain-Analyzer/Finecharts/js/settings/transactions.json', 'w') as f:
-        f.write("jsonfile = \'" + response.text + "\';")
+        f.write("jsonFile = \'" + response.text + "\';")
     return redirect(IP_ADDRESS_TO_HOSTING_SITE + 
-                    "/ResDB-Chain-Analyzer/ResDB-Chain-Analyzer/index.html")
+                    "/ResDB-Chain-Analyzer/index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
